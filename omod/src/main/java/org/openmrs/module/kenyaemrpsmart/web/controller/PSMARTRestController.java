9fee36ec3f48883720a70effd5327e19d0404cdb
ojwanganto <ojwangwachiaje@gmail.com>
2018-02-27 21:50:06 +0300

    fixed integration of openmrs webservices rest module

--- omod/src/main/java/org/openmrs/module/kenyaemrpsmart/web/controller/PSMARTRestController.java
+++ omod/src/main/java/org/openmrs/module/kenyaemrpsmart/web/controller/PSMARTRestController.java
@@ -16,23 +16,43 @@ package org.openmrs.module.kenyaemrpsmart.web.controller;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openmrs.api.context.Context;
+import org.openmrs.module.webservices.rest.SimpleObject;
+import org.openmrs.module.webservices.rest.web.RestConstants;
+import org.openmrs.module.webservices.rest.web.v1_0.controller.BaseRestController;
 import org.openmrs.module.webservices.rest.web.v1_0.controller.MainResourceController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.WebRequest;
 
 /**
  * The main controller.
  */
 @Controller
-public class KenyaEMRPSMARTResourceController extends MainResourceController {
+@RequestMapping(value = "/rest/" + RestConstants.VERSION_1 + "/smartcard")
+public class PSMARTRestController extends BaseRestController {
 	
 	protected final Log log = LogFactory.getLog(getClass());
+
+	@RequestMapping(method = RequestMethod.POST, value = "/receiveshr")
+	@ResponseBody
+	public Object receiveSHR(WebRequest request) {
+
+		return new SimpleObject().add("sessionId", request.getSessionId()).add("authenticated", Context.isAuthenticated());
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/prepareshr")
+	@ResponseBody
+	public Object prepareSHR(WebRequest request) {
+		return new SimpleObject().add("sessionId", request.getSessionId()).add("authenticated", Context.isAuthenticated());
+	}
 
 	/**
 	 * @see org.openmrs.module.webservices.rest.web.v1_0.controller.BaseRestController#getNamespace()
 	 */
+
 	@Override
 	public String getNamespace() {
 		return "v1/kenyaemrpsmart";
